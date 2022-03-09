07434ef2b83ad5c7af33a15d50ec4cdc293c033a
danny-molnar <danny.molnar@cloudreach.com>
2022-03-09 16:26:54 +0000

    update-using vars

--- main.tf
+++ main.tf
@@ -16,6 +16,5 @@ resource "aws_instance" "app_server" {
   instance_type = "t2.micro"
 
   tags = {
-    Name = "ExampleLinuxInstance"
+    Name = var.instance_name
   }
-}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "instance_name" {
+    description = "This variable stores the name of the instance"
+    type = string
+    default = "ExampleLinuxInstance"
+}
