791f5e70d8a58dfd3065b303a46a62f4adb6b630
danny-molnar <danny.molnar@cloudreach.com>
2022-03-09 16:07:28 +0000

    update - instance deployment

--- main.tf
+++ main.tf
@@ -5,8 +5,17 @@ resource "aws_s3_bucket" "my_state_bucket" {
   bucket = "pdev-danny-tfstates"
 
   tags = {
-    Name        = "pdev-danny-tfstates"
+    Name        = "pdev-danny-tfstates" #this tag is the exact name of the bucket
     Environment = "Test"
   }
 }
 
+#in this case the ID for the instance below will be aws_instance.app_server
+resource "aws_instance" "app_server" {
+  ami = "ami-0db188056a6ff81ae"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleLinuxInstance"
+  }
+}
